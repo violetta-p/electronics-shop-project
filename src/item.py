fd05fbd3690ef7466b28f76d04d87b59bb779b97
ViolettaP <violetta.pukhova@mail.ru>
2023-06-08 00:25:20 +0300

    HW_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,10 @@
+import csv
+import os
+from pathlib import Path
+
+FILE_PATH = Path(__file__).parent.parent
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +20,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-
-        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +36,34 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(self.__name) <= 10:
+            self.__name = name
+        else:
+            print('Exception: Длина наименования товара превышает 10 символов')
+
+    @staticmethod
+    def string_to_number(number):
+        num = 0
+        if "." in number:
+            num = float(number)
+        elif "." not in number:
+            num = int(number)
+        return num
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        full_path_to_data = os.path.join(FILE_PATH, "items.csv")
+        with open(full_path_to_data, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for item in reader:
+                name = item["name"]
+                price = cls.string_to_number(item["price"])
+                quantity = cls.string_to_number(item["quantity"])
+                Item.all.append(cls(name, price, quantity))
--- tests/test_item.py
+++ tests/test_item.py
@@ -15,3 +15,16 @@ def test_apply_discount(example):
     Item.pay_rate = 0.5
     example.apply_discount()
     assert example.price == 5000.0
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5.5
